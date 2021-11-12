b0fc9237e9ad03ea44e70db658414220bbea56bd
NicoBren <89282630+NicoBren@users.noreply.github.com>
2021-11-11 21:34:58 -0300

    Update source.ino

--- source.ino
+++ source.ino
@@ -4,16 +4,16 @@
 const byte ROWS = 9; // 9 Linhas - KO0~KO8 
 const byte COLS = 6; // 6 Colunas - KI2~KI7
 // more info about ROWS and COLS in Readme With Service Manual's Prints. 
-char keys[COLS][ROWS] = {
-  {36, 37, 38, 39, 40, 41}, // First Key = C2 (MIDI Number 36)
-  {42, 43, 44, 45, 46, 47},
-  {48, 49, 50, 51, 52, 53},
-  {54, 55, 56, 57, 58, 59},
-  {60, 61, 62, 63, 64, 65},
-  {66, 67, 68, 69, 70, 71},
-  {72, 73, 74, 75, 76, 77},
-  {78, 79, 80, 81, 82, 83},
-  {84, 85, 86, 87, 88, 89} // Last Key = C6 (MIDI Number 84)
+char keys[ROWS][COLS] = {
+  {36, 44, 52, 60, 68, 76},
+  {37, 45, 53, 61, 69, 77},
+  {38, 46, 54, 62, 70, 78},
+  {39, 47, 55, 63, 71, 79},
+  {40, 48, 56, 64, 72, 80},
+  {41, 49, 57, 65, 73, 81},
+  {42, 50, 58, 66, 74, 82},
+  {43, 51, 59, 67, 75, 83},
+  {84, 00, 00, 00, 00, 00} 
 }; 
 // more info about keys in Readme With Service Manual's Prints. - Keys can be charged, changing MIDI Numbers.
 byte rowPins[ROWS] = {22, 24, 26, 28, 30, 32, 34, 36, 38}; //connect to the row pinouts of the kpd - Arduino MEGA
